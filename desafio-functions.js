f3bf28640ba56587ba91498d56325ad5402ca6f6
flaviociriaco <flaviociriaco@yahoo.com>
2022-07-14 21:39:37 -0300

    altercao1

--- desafio-functions.js
+++ desafio-functions.js
@@ -69,67 +69,48 @@
 /*7.Faça um programa onde, você chame uma função que calcule a taxa mensal de juros de um investimento.*/
 
 
+
+
+
 /*8.Você deve enviar como argumentos da função - Valor inicial investido - Valor atual do investimento - Tempo em meses, que o valor está investidoA função deve RETORNAR a taxa de juros, já formatada. Ex: 2,5%. Siga a formula desse vídeo para te auxiliar com o cálculo.*/
 
 // function calculate( valorInicial, valorAtual, tempo){
 //     const juros = (valorAtual - valorInicial) / (valorInicial * tempo)
 //     console.log((juros * 100).toFixed(2) + '%')
 // }
-// calculate(1500,2500,10)
+// calculate(1000,1200,10)
 
 /*1.Escreva um programa onde, você cria uma função geradora de desconto.*/
 
+//  function desconto(valor,valorDesconto){
+//     total = (valor * valorDesconto) / 100
+//     return console.log(`O total com desconto é R$${total}`)
+//  }
+
+//  desconto(2000,10)
 
 /*2.A função aceita 4 parâmetros (Nome do cliente, valor total da compra, um booleano que diz se é a primeira compra do cliente ou não, um booleando que diz se o cliente vai pagar a vista ou não)*/
 
-/*3.Os descontos funcionam da seguinte forma:
-
-4.Se é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 30% de desconto
-
-5.Se é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 25% de desconto
-
-6.Se é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 20%.
-
-7.Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto - Se é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto.
-
-8.Se é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => desconto de 10%.
-
-9.Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for maior que R$ 1000 => 20% de desconto
-
-10.Se NÃO é a primeira compra do cliente, pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 15% de desconto.
-
-11.Se NÃO é a primeira compra do cliente, pagar a vista e a compra for menor que R$ 500 => desconto de 10%.
-
-12.Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for maior que R$ 1000 => 10% de desconto - Se NÃO é a primeira compra do cliente, NÃO pagar a vista, e a compra for menor que R$ 1000, e maior que R$ 500 => 5% de desconto.
-
-13.Se NÃO é a primeira compra do cliente, NÃO pagar a vista e a compra for menor que R$ 500 => SEM DESCONTO- A função deve imprimir na tela:
-
-14.Se o cliente recebeu desconto, imprima na tela um agradecimento pela compra, o valor total sem desconto, o valor total com desconto e a porcentagem de desconto que ele recebeu.
-
-15.Se o cliente não recebeu desconto, imprima na tela um agradecimento pela compra, o valor total da sua compra e um cupom de desconto. Esse cupom deve ser um valor aleatório entre 10 e 20% para a próxima compra.*/
-
-
-function calculoDeDesconto(nome,valor,primeiraCompra,pagamentoAVista){
+function calculoDeDesconto(nome, valor, primeiraCompra, pagamentoAVista) {
     
-    if(primeiraCompra && pagamentoAVista){
-        
-        if(valor > 1000){ console.log(`${nome} você recebeu um desconto de 30% Total R$ ${valor * 0.70}`)}
-        else if(valor < 1000 && valor > 500){console.log(`Você recebeu um desconto de 25% Total R$ ${valor * 0.75}`)}
+    if (primeiraCompra && pagamentoAVista) {
+
+        if (valor > 1000) { console.log(`${nome} você recebeu um desconto de 30% Total R$ ${(valor * 0.70).toFixed(2)}`) }
+        else if (valor < 1000 && valor > 500) { console.log(`Você recebeu um desconto de 25% Total R$ ${valor * 0.75}`) }
+        else if (valor < 500) { console.log(`Você recebeu um desconto de 20% Total R$ ${valor * 0.80}`)}
     }
 
-    if(primeiraCompra && !pagamentoAVista){
-        
+    if (primeiraCompra && !pagamentoAVista) {
+
     }
 
-    if(!primeiraCompra && pagamentoAVista){
-        
+    if (!primeiraCompra && pagamentoAVista) {
+
     }
 
-    if(!primeiraCompra && !pagamentoAVista){
-        
+    if (!primeiraCompra && !pagamentoAVista) {
+
     }
 }
-calculoDeDesconto(flavio,1500,primeiraCompra<pagamentoAVista)
-
-
+calculoDeDesconto("flavio", 2543, true, true)
 
